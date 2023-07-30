ee8e49ff7aa7d599dfb4564967815aabfb7cb9c7
Mohammed Abul Hashem <mdhashem1173@gmail.com>
2023-07-30 19:34:35 -0400

    Main Java class
    while  Loop

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+  public static void main(String[] args){
+      int i = 0;
+      while (i < 5) {
+
+          System.out.println(i); // The code in the loop will run over and over variable(i) less 5
+          i++;
+      }
+
+  }
+
+
+}
